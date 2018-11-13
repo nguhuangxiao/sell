00d99c973ca608986252df3ef36ff8ed1a61806f
nguhuangxiao <761131704@qq.com>
2018-11-13 21:38:00 +0800

    1

--- src/main/java/com/nughuangxiao/sell/dataobj/ProductInfo.java
+++ src/main/java/com/nughuangxiao/sell/dataobj/ProductInfo.java
@@ -1,4 +1,42 @@
 package com.nughuangxiao.sell.dataobj;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class ProductInfo {
+
+    @Id
+    private String productId;
+
+    /** 商品名称 **/
+    private String productName;
+
+    /** 库存 **/
+    private Integer productStock;
+
+    /** 描述 **/
+    private String productDescription;
+
+    /** 小图 **/
+    private String productIcon;
+
+    /** 类目编号 **/
+    private Integer categoryType;
+
+    /** 类目状态 **/
+    private Integer productStatus;
+
+    public ProductInfo(String productId, String productName, Integer productStock, String productDescription, String productIcon, Integer categoryType, Integer productStatus) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productStock = productStock;
+        this.productDescription = productDescription;
+        this.productIcon = productIcon;
+        this.categoryType = categoryType;
+        this.productStatus = productStatus;
+    }
 }
--- src/test/java/com/nughuangxiao/sell/repository/ProductInfoRepositoryTest.java
+++ src/test/java/com/nughuangxiao/sell/repository/ProductInfoRepositoryTest.java
@@ -1,5 +1,38 @@
-import static org.junit.Assert.*;
+package com.nughuangxiao.sell.repository;
 
+import com.nughuangxiao.sell.dataobj.ProductInfo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ProductInfoRepositoryTest {
 
+    @Autowired
+    private ProductInfoRepository repository;
+
+    @Test
+    public void saveTest() {
+        ProductInfo productInfo = new ProductInfo(
+                "1",
+                "皮蛋粥",
+                100,
+                "大众点评热销",
+                "http://www.baidu.com",
+                2,
+                1
+        );
+        repository.save(productInfo);
+    }
+
+    @Test
+    public void findByProductStatus() {
+
+
+
+    }
 }
