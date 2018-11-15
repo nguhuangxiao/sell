b0224b5c273d6115d90a35b6d49ddb844264a331
nguhuangxiao <761131704@qq.com>
2018-11-15 08:38:44 +0800

    service 2

--- src/test/java/com/nughuangxiao/sell/service/impl/ProductServiceImplTest.java
+++ src/test/java/com/nughuangxiao/sell/service/impl/ProductServiceImplTest.java
@@ -1,10 +1,18 @@
 package com.nughuangxiao.sell.service.impl;
 
+import com.nughuangxiao.sell.dataobj.ProductInfo;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -16,19 +24,44 @@ public class ProductServiceImplTest {
     private ProductServiceImpl productService;
 
     @Test
-    public void findOne() {
+    public void findOne() throws Exception {
+
+        ProductInfo productInfo = productService.findOne("2");
+        System.out.println(productInfo);
 
     }
 
     @Test
-    public void findUpAll() {
+    public void findUpAll() throws Exception{
+
+        List<ProductInfo> productInfoList = productService.findUpAll();
+        Assert.assertNotEquals(0, productInfoList.size());
     }
 
     @Test
-    public void findAll() {
+    public void findAll() throws Exception{
+
+        PageRequest request = new PageRequest(0, 10);
+
+        Page<ProductInfo> productInfoList = productService.findAll(request);
+
+        System.out.println(productInfoList.getTotalElements());
+
     }
 
     @Test
-    public void save() {
+    public void save() throws Exception{
+        ProductInfo productInfo = new ProductInfo(
+                "4",
+                "huangxiao",
+                new BigDecimal(10.5),
+                99,
+                "很好玩到东西",
+                "http://baidu.com",
+                1,
+                3
+        );
+
+        productService.save(productInfo);
     }
 }
